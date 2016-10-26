5c00de70255c3d38dbb47f7efc5029ff13c255b9
zuriko <znatr10@freeuni.edu.ge>
2016-10-26 17:16:04 +0400

    Sign in

--- Mich/LoginViewController.swift
+++ Mich/LoginViewController.swift
@@ -25,24 +25,7 @@ class LoginViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButt
 
     override func viewDidLoad() {
         super.viewDidLoad()
-/*
-        loginTF.delegate = self
-        passwordTF.delegate = self
-        loginTF.backgroundColor = UIColor (red:236 / 255.0, green:68 / 255.0, blue:45 / 255.0, alpha:0.5)
-        passwordTF.backgroundColor = UIColor (red:236 / 255.0, green:68 / 255.0, blue:45 / 255.0, alpha:0.5)
-        loginTF.attributedPlaceholder = NSAttributedString(string:"Email", attributes:[NSForegroundColorAttributeName: UIColor(red: 1, green: 1, blue: 1, alpha: 1)])
-        passwordTF.attributedPlaceholder = NSAttributedString(string:"Password", attributes:[NSForegroundColorAttributeName: UIColor(red: 1, green: 1, blue: 1, alpha: 1)])
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.dismissKeyboard))
-        view.addGestureRecognizer(tap)
-        
-        if(UIDevice.current.userInterfaceIdiom == .pad ){
-            
-            print("Ipod")
-        }else{
-            print("another device")
-        }
-*/
+
 //////// facebook login
         self.loginFacebookBTN.delegate = self
         loginFacebookBTN.readPermissions = ["public_profile", "email", "user_friends"]
@@ -127,48 +110,5 @@ class LoginViewController: UIViewController, UITextFieldDelegate, FBSDKLoginButt
     }
 
 
-
-    
-    
-
-/*
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-       
-        scrollView.setContentOffset(CGPoint(x:0, y:250), animated: true)
-       
-
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        
-        scrollView.setContentOffset(CGPoint(x:0, y:0), animated: true)
-    }
-
-     
-     func dismissKeyboard() {
-     //Causes the view (or one of its embedded text fields) to resign the first responder status.
-     view.endEditing(true)
-     }
-     
-     
-     
-     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-     textField.resignFirstResponder()
-     return true
-     }
-     
-     
-*/
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
