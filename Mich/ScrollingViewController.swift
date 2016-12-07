30181f34ecca615a7231c0eb827c856e7505578e
spiderbatman <gigi.pataraia@gmail.com>
2016-12-07 18:21:44 +0400

    scrolling menu auto scroll

--- Mich/ScrollingViewController.swift
+++ Mich/ScrollingViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 
-class ScrollingViewController: UIViewController, UIGestureRecognizerDelegate {
+class ScrollingViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        scrollView.delegate = self
     }
-
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if (!decelerate) {
+            if (scrollView.contentOffset.x >= 120) {
+                scrollView.setContentOffset(CGPoint(x: 240, y: 0), animated: true)
+            }
+            else {
+                scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+            }
+        }
+    }
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        //print(velocity.x)
+        if (velocity.x > 0) {
+            scrollView.setContentOffset(CGPoint(x: 240, y: 0), animated: true)
+            //print(scrollView.contentOffset.x)
+        }
+        else if (velocity.x < 0){
+            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+            //print(scrollView.contentOffset.x)
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
