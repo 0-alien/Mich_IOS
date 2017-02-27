84057b040f368971b63d6342a74f48d2ca866d28
zuriko <znatr10@freeuni.edu.ge>
2017-02-27 12:53:41 +0400

    addComment

---
+++ Mich/AddCommentResponse.swift
@@ -0,0 +1,23 @@
+//
+//  AddCommentResponse.swift
+//  Mich
+//
+//  Created by zuraba on 2/27/17.
+//  Copyright © 2017 Gigi. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class AddCommentResponse: Mappable {
+    
+    
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+    }
+}
--- Mich/MichTransport.swift
+++ Mich/MichTransport.swift
@@ -1196,6 +1196,60 @@ class MichTransport {
     
     
     
+    static func addcomment(token: String, postID: Int, comment: String, successCallbackForAddComment: @escaping () -> Void, errorCallbackForAddComment: @escaping (DefaultError) -> Void ){
+        
+        let reqString = BASE_URL + "post/comment"
+        
+        let addcommentRequest = AddCommentRequest(token: token, postID: postID, comment: comment)
+        let payloadJson = addcommentRequest.toJSONString()
+        
+        
+        
+        Alamofire.request(reqString, method: .post, parameters: [:], encoding: payloadJson!).responseString { response in
+            
+            
+            if( response.result.isSuccess ){
+                
+                let JString = "\(response.result.value!)"
+                print(JString)
+                let baseResponse = BaseResponse<AddCommentResponse>(JSONString: JString)
+                
+                if baseResponse!.code! == SUCCESS_CODE {
+                    
+                    
+                    successCallbackForAddComment()
+                    
+                }else{
+                    
+                    print(baseResponse!.message!)
+                    
+                    let error = DefaultError()
+                    error.errorString = baseResponse!.message!
+                    
+                    
+                    errorCallbackForAddComment(error)
+                    
+                }
+                
+                
+            }else{
+                
+                let error = DefaultError()
+                error.errorString = "Something went wrong!"
+                
+                
+                errorCallbackForAddComment(error)
+                
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
     /////////////// search users
     
     
